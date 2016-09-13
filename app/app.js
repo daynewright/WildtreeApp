c5ea6cfeecdc7adffc1723cabc598af78f6d4d6a
Dayne Wright <daynewr@gmail.com>
2016-09-13 15:51:41 -0500

    worked more on boilerplate files for authorization with FB

--- app/app.js
+++ app/app.js
@@ -1,3 +1,26 @@
 'use strict';
 
-let app = angular.module('MyApp', ['ngRoute']);
+let app = angular.module('MyApp', ['ngRoute'])
+          .constant('FirebaseURL', 'https://wildtree-app.firebaseio.com/');
+
+let isAuth = (AuthFactory)=> new Promise((resolve, reject)=>{
+  // This will be a boolean and it will resolve if its true, meaning you can access the URLs below
+  if(AuthFactory.isAuthenticated()){
+    resolve();
+  } else {
+    reject();
+  }
+});
+
+
+app.config(($routeProvider)=> {
+  $routeProvider
+  .when('/', {
+    templateUrl: 'partials/Login.html',
+    controller: 'LoginCtrl'
+  });
+});
+
+app.run((FbCreds)=> {
+  firebase.initializeApp(FbCreds);
+});
