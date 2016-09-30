8c693fcf05e6ab9ea4341a1f6dc000b43d7d42af
Dayne Wright <daynewr@gmail.com>
2016-09-30 15:20:54 -0500

    added logic to show correct user information in conversation

--- app/controllers/MessagesCtrl.js
+++ app/controllers/MessagesCtrl.js
@@ -1,15 +1,39 @@
 'use strict';
 
 app.controller('MessagesCtrl', function($scope, $routeParams, $q, $uibModal, AuthFactory, ConversationFactory){
+  let user = AuthFactory.getUserId();
 
   $scope.showSpinner = true;
 
-  ConversationFactory.getAllConversationsForUser(AuthFactory.getUserId())
+  $scope.loggedInUser = user;
+
+  ConversationFactory.getAllConversationsForUser(user)
   .then((conversations)=> {
     conversations.selected = conversations[0];
     $scope.conversations = conversations;
     $scope.showSpinner = false;
   });
+
+  $scope.unreadCount = (conversation)=> {
+    let count = 0;
+    conversation.messages.forEach((message) => {
+      if(message.authorId !== user && !message.read){
+        count++;
+      }
+    });
+    return count;
+  };
+
+  $scope.otherUser = (users)=> {
+    let correctUser = {};
+    users.forEach((u)=> {
+      if(u.userId !== user){
+        correctUser.name = u.name;
+        correctUser.photo = u.photo;
+      }
+    });
+    return correctUser;
+  };
 
   $scope.formatDate = (messageDate)=> {
     messageDate = moment(messageDate).startOf('minute').fromNow();
