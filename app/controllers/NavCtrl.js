7c16d17efd7f9f830b84f519a671547571c8f801
Dayne Wright <daynewr@gmail.com>
2016-10-01 16:15:26 -0500

    added watch for notification on navbar

--- app/controllers/NavCtrl.js
+++ app/controllers/NavCtrl.js
@@ -3,7 +3,30 @@
 app.controller('NavCtrl', function($scope, $window, $location, $q, AuthFactory, UserFactory, ConversationFactory) {
 
 
+  let CONVERSATIONREF = firebase.database().ref('conversations');
 
+  CONVERSATIONREF.on('value', (snapshot)=> {
+    UserFactory.getUser(AuthFactory.getUserId())
+    .then((user) => {
+      $scope.user = user;
+      $scope.isButcher = user.isButcher;
+      return $q.resolve(user);
+    })
+    .then((user)=> {
+       return ConversationFactory.getAllConversationsForUser(user.userId);
+    })
+    .then((conversations)=> {
+      let count = 0;
+      conversations.forEach((conversation)=> {
+        conversation.messages.forEach((message)=> {
+          if(message.authorId !== $scope.user.userId && !message.read){
+            count++;
+          }
+        });
+      });
+      $scope.count = count;
+    });
+  });
 
   // Logs out current user
   $scope.logout = () => {
