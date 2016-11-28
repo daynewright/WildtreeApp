002567206e346c7647664386bb4644a8b202ec28
Dayne Wright <daynewr@gmail.com>
2016-11-27 21:40:35 -0600

    added dependencies

---
+++ app/values/FbCreds.js
@@ -0,0 +1,7 @@
+"use strict";
+
+app.constant('FbCreds', {
+ apiKey: "AIzaSyC4k95Gwfjf5q_8P7VsIN75YXq_oSwYIrQ",
+ authDomain: "wildtree-app.firebaseapp.com",
+ databaseURL: "https://wildtree-app.firebaseio.com"
+});
